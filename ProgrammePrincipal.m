c9b34709831467f2ef4a53c0e39feba8244f636e
jocelyndubuc <jocelyndubuc@localhost>
2022-04-08 10:13:39 -0400

    Ajout des commentaires programme principal

--- ProgrammePrincipal.m
+++ ProgrammePrincipal.m
@@ -5,6 +5,8 @@ function ProgrammePrincipal()
 
     %% Main
 
+    %Execution des etapes requises (creation d'une plateforme, generation
+    %du rapport, analyse des transactions, generation d'un second rapport.
     plateforme = Plateforme();
     plateforme.GenererRapport('RapportAvant');
     plateforme.AnalyserJournalTransactions('Transactions.txt');
--- ProgrammeTest.m
+++ ProgrammeTest.m
@@ -1,23 +1,18 @@
 
 function ProgrammeTest()
-ConfigurerProjet;
-
-format long g;
-rng(0);
-client1 = Client('charles','barette','123456789');
-Jocelyn = Client('Jocelyn','Dubuc','827891898');
-comptePatate1 = Compte(1000,1000);
-comptePouletBeurre1 = Compte(1000,1000);
-
-celideJocelyn = Compte(2000,2000);
-
-dejardounes = Banque('dejardounes','4');
-dejardounes.AjouterClient(Jocelyn);
-dejardounes.AjouterCompte(celideJocelyn,Jocelyn);
-
-plateforme = Plateforme();
-plateforme.GenererRapport('RapportAvant');
-plateforme.AnalyserJournalTransactions('Transactions.txt');
-plateforme.GenererRapport('RapportApres');
+    
+    %Configuration du projet
+    ConfigurerProjet;
+    
+    %Fixer la semence aleatoire
+    format long g;
+    rng(0);
+    
+    %Execution des etapes requises (creation d'une plateforme, generation
+    %du rapport, analyse des transactions, generation d'un second rapport.
+    plateforme = Plateforme();
+    plateforme.GenererRapport('RapportAvant');
+    plateforme.AnalyserJournalTransactions('Transactions.txt');
+    plateforme.GenererRapport('RapportApres');
 
 end
